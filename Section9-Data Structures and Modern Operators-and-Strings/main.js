58973186edba419631cb16aacb898ed0da137904
Huy nt <huyntph27481@fpt.edu.vn>
2023-02-04 01:18:20 +0700

    sửa tên file

--- Section9-Data Structures and Modern Operators-and-Strings/main.js
+++ Section9-Data Structures and Modern Operators-and-Strings/main.js
@@ -67,42 +67,42 @@ const restaurant = {
 // 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
 // 3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
 // 4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
-//       [FIRST HALF] 17: GOAL
+//   [FIRST HALF] 17: GOAL
 
-const gameEvents = new Map([
-    [17, ' GOAL'],
-    [36, ' Substitution'],
-    [47, ' GOAL'],
-    [61, 'Substitution'],
-    [64, ' Yellow card'],
-    [69, ' Red card'],
-    [70, ' Substitution'],
-    [72, ' Substitution'],
-    [76, ' GOAL'],
-    [80, ' GOAL'],
-    [92, ' Yellow card'],
-]);
+// const gameEvents = new Map([
+//     [17, ' GOAL'],
+//     [36, ' Substitution'],
+//     [47, ' GOAL'],
+//     [61, 'Substitution'],
+//     [64, ' Yellow card'],
+//     [69, ' Red card'],
+//     [70, ' Substitution'],
+//     [72, ' Substitution'],
+//     [76, ' GOAL'],
+//     [80, ' GOAL'],
+//     [92, ' Yellow card'],
+// ]);
 
 
-// 1.
-const events = [...new Set(gameEvents.values())];
-console.log(events);
-// 2.
-gameEvents.delete(64);
-// 3.
-console.log(
-  `An event happened, on average, every ${90 / gameEvents.size} minutes`
-);
-const time = [...gameEvents.keys()].pop();
-console.log(time);
-console.log(
-  `An event happened, on average, every ${time / gameEvents.size} minutes`
-);
-// 4.
-for (const [min, event] of gameEvents) {
-  const half = min <= 45 ? 'FIRST' : 'SECOND';
-  console.log(`[${half} HALF] ${min}: ${event}`);
-}
+// // 1.
+// const events = [...new Set(gameEvents.values())];
+// console.log(events);
+// // 2.
+// gameEvents.delete(64);
+// // 3.
+// console.log(
+//   `An event happened, on average, every ${90 / gameEvents.size} minutes`
+// );
+// const time = [...gameEvents.keys()].pop();
+// console.log(time);
+// console.log(
+//   `An event happened, on average, every ${time / gameEvents.size} minutes`
+// );
+// // 4.
+// for (const [min, event] of gameEvents) {
+//   const half = min <= 45 ? 'FIRST' : 'SECOND';
+//   console.log(`[${half} HALF] ${min}: ${event}`);
+// }
 
 
 // 119. Summary: Which Data Structure to Use?
